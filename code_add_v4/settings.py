64e8965a1fb9690d8cfd871a393757882ac25f4a
TmKUz0790 <104360495+TmKUz0790@users.noreply.github.com>
2024-08-17 11:14:11 +0500

    ttt67

--- code_add_v4/settings.py
+++ code_add_v4/settings.py
@@ -74,7 +74,9 @@ WSGI_APPLICATION = 'code_add_v4.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3'
+    )
 }
 
 # Password validation
