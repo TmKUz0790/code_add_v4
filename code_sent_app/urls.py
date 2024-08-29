debdf26e16c48c22070295c9c8342e225f70d3f7
TmKUz0790 <104360495+TmKUz0790@users.noreply.github.com>
2024-08-29 14:12:48 +0500

    t2

--- code_sent_app/urls.py
+++ code_sent_app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('send-serial-numbers/', send_serial_numbers, name='send_serial_numbers'),
-    path('home/', home, name='home'),
+    #path('home/', home, name='home'),
     path('', CustomLoginView.as_view(), name='login'),
 
 ]
